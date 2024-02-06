85bb9016250b5f8463cc77556448a7b368e65fc0
Leonardo Venâncio Correia <leonardovenanciocorreia@gmail.com>
2024-02-06 14:24:46 -0300

    Atributos e métodos da classe Bill feitos

---
+++ 06bar/src/entities/Bill.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class Bill {
+	
+	public char gender;
+	public int beer, barbecue, softDrink;
+
+	public static double feeding() {
+		
+	}
+	
+	public static double cover() {
+		
+	}
+	
+	public static double ticket() {
+		
+	}
+	
+	public static double total() {
+		
+	}
+
+}
