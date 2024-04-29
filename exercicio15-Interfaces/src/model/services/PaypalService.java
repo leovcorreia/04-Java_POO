0feec7266281c4e08016a7291b49d532135fd2e0
Leonardo Venâncio Correia <leonardovenanciocorreia@gmail.com>
2024-04-29 16:11:41 -0300

    Initial implementation

---
+++ exercicio15-Interfaces/src/model/services/PaypalService.java
@@ -0,0 +1,13 @@
+package model.services;
+
+public class PaypalService implements OnlinePaymentService {
+	
+	public Double paymentFee(Double amount) {
+		return 0.0;
+	}
+	
+	public Double interest(Double amount, Integer months) {
+		return 0.0;
+	}
+
+}
