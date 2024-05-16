6841ce699e135543d45ab998eb474be6821e1962
Leonardo Venâncio Correia <leonardovenanciocorreia@gmail.com>
2024-05-16 15:07:57 -0300

    Initial implementation
    Reads data from input file and put into the Map structure

---
+++ exercicio16-Generics(Map)/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Map<String, Integer> elections = new HashMap<>();
+		
+		System.out.println("Enter file full path: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			
+			while (line != null) {
+				String[] fields = line.split(",");
+				String name = fields[0];
+				Integer votes = Integer.parseInt(fields[1]);
+				elections.put(name, votes);
+			}
+			
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		sc.close();
+	}
+
+}
