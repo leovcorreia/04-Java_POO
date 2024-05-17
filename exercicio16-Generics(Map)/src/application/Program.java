0629da337c723487e458e00d4570028b24b911b2
Leonardo Venâncio Correia <leonardovenanciocorreia@gmail.com>
2024-05-16 22:09:27 -0300

    Final implementation (my version)
    Program that reads votes from an election into an input file and returns the total number of votes each candidate had

--- exercicio16-Generics(Map)/src/application/Program.java
+++ exercicio16-Generics(Map)/src/application/Program.java
@@ -3,10 +3,10 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Program {
 
@@ -15,7 +15,7 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Map<String, Integer> elections = new HashMap<>();
+		Map<String, Integer> elections = new TreeMap<>();
 		
 		System.out.println("Enter file full path: ");
 		String path = sc.nextLine();
@@ -27,12 +27,26 @@ public class Program {
 				String[] fields = line.split(",");
 				String name = fields[0];
 				Integer votes = Integer.parseInt(fields[1]);
-				elections.put(name, votes);
+				
+				if (elections.containsKey(name)) {
+					elections.put(name, elections.get(name) + votes);
+				} else {
+					elections.put(name, votes);
+				}
+				
+				line = br.readLine();
+			}
+			
+			for (String key: elections.keySet()) {
+				System.out.println(key + ": " + elections.get(key));
 			}
 			
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("RunTimeExceptionError: " + e.getMessage());
 		}
 		
 		sc.close();
