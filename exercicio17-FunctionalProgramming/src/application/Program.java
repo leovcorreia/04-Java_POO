784393ef373d7d3461a941764618a1fad22c1598
Leonardo Venâncio Correia <leonardovenanciocorreia@gmail.com>
2024-06-11 17:38:01 -0300

    Final implementation with Streams (pipeline)
    Program that reads employee data from a file and shows, in alphabetical order, the email of employees whose salary is bigger than a value informed by the user; also shows the sum of the salary of employees whose name begins with the letter 'M'

--- exercicio17-FunctionalProgramming/src/application/Program.java
+++ exercicio17-FunctionalProgramming/src/application/Program.java
@@ -1,7 +1,15 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entities.Employee;
 
 public class Program {
 
@@ -10,7 +18,45 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		System.out.print("Enter salary: ");
+		Double salaryToBeComputed = sc.nextDouble();
 		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			List<Employee> emp = new ArrayList<>();
+			
+			// READING THE FILE
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				emp.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+				
+				line = br.readLine();
+			}
+			
+			List<String> emails = emp.stream()
+								.filter(x -> x.getSalary() > salaryToBeComputed)
+								.map(x -> x.getEmail())
+								.sorted()
+								.collect(Collectors.toList());
+			
+			System.out.println("Email of people whose salary is more than " + String.format("%.2f", salaryToBeComputed) + ":");
+			emails.forEach(System.out::println);
+			
+			double sum = emp.stream()
+						.filter(x -> x.getName().charAt(0) == 'M')
+						.map(x -> x.getSalary())
+						.reduce(0.0, (x, y) -> x + y);
+			
+			System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", sum));
+			
+		} catch(RuntimeException e) {
+			System.out.println("Unexpected error: " + e);
+		} catch(IOException e) {
+			System.out.println("Error: " + e);
+		}
 		
 		sc.close();
 
