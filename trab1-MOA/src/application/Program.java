bb4797f66ebf0b2c7abf02e34c486780b8539c34
Leonardo Venâncio Correia <leonardovenanciocorreia@gmail.com>
2024-08-06 11:47:19 -0300

    Tentando montar a função objetivo

--- trab1-MOA/src/application/Program.java
+++ trab1-MOA/src/application/Program.java
@@ -21,39 +21,47 @@ public class Program {
 		try(BufferedReader br = new BufferedReader(new FileReader(path))){
 			
 			String line = br.readLine();
-			while (line != null) {
-				// LÊ A QUANTIDADE DE MOCHILAS
-				int qtdMochilas = Integer.parseInt(line);
-				int[] capacidadesMochila = new int[qtdMochilas];
+			// LÊ A QUANTIDADE DE MOCHILAS
+			int qtdMochilas = Integer.parseInt(line);
+			int[] capacidadesMochila = new int[qtdMochilas];
 				
-				// LÊ A QUANTIDADE DE ITENS
+			// LÊ A QUANTIDADE DE ITENS
+			line = br.readLine();
+			int qtdItens = Integer.parseInt(line);
+				
+			// LÊ A CAPACIDADE DE CADA MOCHILA E GUARDA EM UM VETOR
+			for (int i = 0; i < qtdMochilas; i++) {
 				line = br.readLine();
-				int qtdItens = Integer.parseInt(line);
-				
-				// LÊ A CAPACIDADE DE CADA MOCHILA E GUARDA EM UM VETOR
-				for (int i = 0; i < qtdMochilas; i++) {
-					line = br.readLine();
-					capacidadesMochila[i] = Integer.parseInt(line);
-				}
-				
-				// LÊ PESO E VALOR DE CADA ITEM, CRIA O OBJETO ITEM E GUARDA EM UM VETOR DO TIPO ITEM
-				Item[] arrayItens = new Item[qtdItens];
-				for (int i = 0; i < qtdItens; i++) {
-					line = br.readLine();
-					String[] fields = line.split("\\s+");
-					
-					int peso = Integer.parseInt(fields[0]);
-					int valor = Integer.parseInt(fields[1]);
-					arrayItens[i] = new Item(peso, valor);
-				}
-				
+				capacidadesMochila[i] = Integer.parseInt(line);
 			}
+				
+			// LÊ PESO E VALOR DE CADA ITEM, CRIA O OBJETO ITEM E GUARDA EM UM VETOR DO TIPO ITEM
+			Item[] arrayItens = new Item[qtdItens];
+			for (int i = 0; i < qtdItens; i++) {
+				line = br.readLine();
+				String[] fields = line.split("\\s+");
+					
+				int peso = Integer.parseInt(fields[0]);
+				int valor = Integer.parseInt(fields[1]);
+				arrayItens[i] = new Item(peso, valor);
+			}
+				
+			int[][] matStatus = new int[qtdItens][qtdMochilas]; 
+			int funcObjetivo = funcaoObjetivo(arrayItens, qtdItens, qtdMochilas, matStatus);
 			
 		} catch (IOException e) {
 			System.out.println("Erro: " + e);
 		}
 		
 		sc.close();
+	}
+	
+	public static int funcaoObjetivo(Item[] arrayItens, int qtdItens, int qtdMochilas, int[][] matStatus) {
+		for (int i = 0; i < ; i++) {
+			for (int j = 0; j < ; j++) {
+				
+			}
+		}
 	}
 
 }
