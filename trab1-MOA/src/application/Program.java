42dffec5df0c2ca1355cf272e4e3d7649feb053f
Leonardo Venâncio Correia <leonardovenanciocorreia@gmail.com>
2024-08-16 19:47:09 -0300

    Versão Bruno que roda alguma coisa

--- trab1-MOA/src/application/Program.java
+++ trab1-MOA/src/application/Program.java
@@ -1,5 +1,11 @@
 package application;
 
+import scpsolver.constraints.LinearBiggerThanEqualsConstraint;
+import scpsolver.constraints.LinearSmallerThanEqualsConstraint;
+import scpsolver.lpsolver.LinearProgramSolver;
+import scpsolver.lpsolver.SolverFactory;
+import scpsolver.problems.LinearProgram;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,169 +16,85 @@ import entities.Item;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Entre com o caminho do arquivo: ");
-		String path = sc.nextLine();
-		
-		try(BufferedReader br = new BufferedReader(new FileReader(path))){
-			
-			String line = br.readLine();
-			// LÊ A QUANTIDADE DE MOCHILAS
-			int qtdMochilas = Integer.parseInt(line);
-			int[] capacidadesMochila = new int[qtdMochilas];
-				
-			// LÊ A QUANTIDADE DE ITENS
-			line = br.readLine();
-			int qtdItens = Integer.parseInt(line);
-				
-			// LÊ A CAPACIDADE DE CADA MOCHILA E GUARDA EM UM VETOR
-			for (int i = 0; i < qtdMochilas; i++) {
-				line = br.readLine();
-				capacidadesMochila[i] = Integer.parseInt(line);
-			}
-				
-			// LÊ PESO E VALOR DE CADA ITEM, CRIA O OBJETO ITEM E GUARDA EM UM VETOR DO TIPO ITEM
-			Item[] arrayItens = new Item[qtdItens];
-			for (int i = 0; i < qtdItens; i++) {
-				line = br.readLine();
-				String[] fields = line.split("\\s+");
-					
-				int peso = Integer.parseInt(fields[0]);
-				int valor = Integer.parseInt(fields[1]);
-				arrayItens[i] = new Item(peso, valor);
-			}
-			
-			// MATRIZ QUE TERÁ 1 CASO ITEM I ESTIVER NA MOCHILA J; TERÁ 0, CASO CONTRÁRIO
-			int[][] matStatus = new int[qtdItens][qtdMochilas]; 
-			
-			String funcObjetivo = montarFuncaoObjetivo(arrayItens, qtdItens, qtdMochilas);
-			System.out.println("Função objetivo: \n" + funcObjetivo);
-			
-			String restricoesCapacidade = montarRestriçõesCapacidade(arrayItens, qtdItens, qtdMochilas, capacidadesMochila);
-            System.out.println("\nRestrições das capacidades das mochilas:\n" + restricoesCapacidade);
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Entre com o caminho do arquivo de entrada: ");
+        String path = sc.nextLine();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             
-            String restricoesUnicidade = montarRestricoesUnicidade(qtdItens, qtdMochilas);
-            System.out.println("\nRestrições de unicidade de cada item:\n" + restricoesUnicidade);
-            
-            String restricoesBinarias = montarRestricoesBinarias(qtdItens, qtdMochilas);
-            System.out.println("\nRestrições binárias:\n" + restricoesBinarias);
-			
-		} catch (IOException e) {
-			System.out.println("Erro: " + e);
-		}
-		
-		sc.close();
-	}
-	
-	public static String montarFuncaoObjetivo(Item[] arrayItens, int qtdItens, int qtdMochilas) {
-		StringBuilder funcObjetivo = new StringBuilder("maximize: ");
-
-        for (int i = 0; i < qtdItens; i++) {
-            for (int j = 0; j < qtdMochilas; j++) {
-                if (i > 0 || j > 0) {
-                    funcObjetivo.append(" + ");
-                }
-                funcObjetivo.append(arrayItens[i].getValue())
-                	.append(" * x")
-                	.append(i)
-                	.append("_")
-                	.append(j);
-                // Exemplo de como ficaria:
-                // maximize: 10 * x0_0 + 10 * x0_1 + 15 * x1_0 + 15 * x1_1
+            String line = br.readLine();
+            int qtdMochilas = Integer.parseInt(line);
+            int[] capacidadesMochila = new int[qtdMochilas];
+                
+            line = br.readLine();
+            int qtdItens = Integer.parseInt(line);
+                
+            for (int i = 0; i < qtdMochilas; i++) {
+                line = br.readLine();
+                capacidadesMochila[i] = Integer.parseInt(line);
             }
+                
+            Item[] arrayItens = new Item[qtdItens];
+            for (int i = 0; i < qtdItens; i++) {
+                line = br.readLine();
+                String[] fields = line.split("\\s+");
+                    
+                int peso = Integer.parseInt(fields[0]);
+                int valor = Integer.parseInt(fields[1]);
+                arrayItens[i] = new Item(peso, valor);
+            }
+            
+            // Criar o problema com SCPSolver
+            int numVariaveis = qtdItens * qtdMochilas;
+            double[] coeficientesObjetivo = new double[numVariaveis];
+            for (int i = 0; i < qtdItens; i++) {
+                for (int j = 0; j < qtdMochilas; j++) {
+                    coeficientesObjetivo[i + j * qtdItens] = arrayItens[i].getValue();
+                }
+            }
+
+            // Criar o LinearProgram com a função objetivo
+            LinearProgram lp = new LinearProgram(coeficientesObjetivo);
+
+            // Adicionar restrições de capacidade
+            for (int j = 0; j < qtdMochilas; j++) {
+                double[] coeficientes = new double[numVariaveis];
+                for (int i = 0; i < qtdItens; i++) {
+                    coeficientes[i + j * qtdItens] = arrayItens[i].getWeight();
+                }
+                lp.addConstraint(new LinearSmallerThanEqualsConstraint(coeficientes, capacidadesMochila[j], "c_mochila" + j));
+            }
+
+            // Adicionar restrições de unicidade
+            for (int i = 0; i < qtdItens; i++) {
+                double[] coeficientes = new double[numVariaveis];
+                for (int j = 0; j < qtdMochilas; j++) {
+                    coeficientes[i + j * qtdItens] = 1;
+                }
+                lp.addConstraint(new LinearSmallerThanEqualsConstraint(coeficientes, 1, "c_unicidade" + i));
+            }
+
+            // Configurar o problema como de maximização
+            lp.setMinProblem(false);  // Maximizar
+
+            // Resolver o problema
+            LinearProgramSolver solver = SolverFactory.newDefault();
+            double[] sol = solver.solve(lp);
+
+            // Exibir a solução
+            System.out.println("Solução: ");
+            for (int i = 0; i < sol.length; i++) {
+                System.out.println("x" + (i / qtdMochilas) + "_" + (i % qtdMochilas) + " = " + sol[i]);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro: " + e);
         }
-
-        return funcObjetivo.toString();
-	}
-	
-	public static String montarRestriçõesCapacidade(Item[] arrayItens, int qtdItens, int qtdMochilas, int[] capacidadesMochilas) {
-	    StringBuilder restricoesCapacidade = new StringBuilder();
-
-	    // Itera sobre cada mochila
-	    for (int j = 0; j < qtdMochilas; j++) {
-	        // Adiciona a restrição para a mochila j
-	        if (j > 0) {
-	            restricoesCapacidade.append("\n");
-	        }
-	        restricoesCapacidade.append("s.a. mochila").append(j).append(": ");
-	        
-	        // Adiciona a soma dos pesos dos itens na mochila j
-	        for (int i = 0; i < qtdItens; i++) {
-	            if (i > 0) {
-	                restricoesCapacidade.append(" + ");
-	            }
-	            restricoesCapacidade.append(arrayItens[i].getWeight()).append(" * x").append(i).append("_").append(j);
-	        }
-	        
-	        // Adiciona a capacidade máxima da mochila
-	        restricoesCapacidade.append(" <= ").append(capacidadesMochilas[j]);
-	        
-	        // Exemplo de comi ficaria:
-	        // s.t. mochila0: 10 * x0_0 + 20 * x1_0 + 15 * x2_0 <= 50
-	        // s.t. mochila1: 10 * x0_1 + 20 * x1_1 + 15 * x2_1 <= 70
-
-	    }
-
-	    return restricoesCapacidade.toString();
-	}
-
-	public static String montarRestricoesUnicidade(int qtdItens, int qtdMochilas) {
-	    StringBuilder restricoesUnicidade = new StringBuilder();
-
-	    // Itera sobre cada item
-	    for (int i = 0; i < qtdItens; i++) {
-	        // Adiciona a restrição para o item i
-	        if (i > 0) {
-	            restricoesUnicidade.append("\n");
-	        }
-	        restricoesUnicidade.append("s.a. unicidade_item").append(i).append(": ");
-	        
-	        // Adiciona a soma das variáveis de decisão para o item i
-	        for (int j = 0; j < qtdMochilas; j++) {
-	            if (j > 0) {
-	                restricoesUnicidade.append(" + ");
-	            }
-	            restricoesUnicidade.append("x").append(i).append("_").append(j);
-	        }
-	        
-	        // Adiciona a restrição de unicidade
-	        restricoesUnicidade.append(" <= 1");
-	        
-	        // Exemplo de como ficaria:
-	        // s.a. unicidade_item0: x0_0 + x0_1 <= 1
-	        // s.a. unicidade_item1: x1_0 + x1_1 <= 1
-	        // s.a. unicidade_item2: x2_0 + x2_1 <= 1
-	    }
-
-	    return restricoesUnicidade.toString();
-	}
-
-	public static String montarRestricoesBinarias(int qtdItens, int qtdMochilas) {
-	    StringBuilder restricoesBinarias = new StringBuilder();
-
-	    for (int i = 0; i < qtdItens; i++) {
-	        for (int j = 0; j < qtdMochilas; j++) {
-	            if (restricoesBinarias.length() > 0) {
-	                restricoesBinarias.append("\n");
-	            }
-	            restricoesBinarias.append("bin x").append(i).append("_").append(j);
-	        }
-	    }
-
-	    return restricoesBinarias.toString();
-	    
-	    /*Exemplo de como ficaria:
-	    bin x0_0
-	    bin x0_1
-	    bin x1_0
-	    bin x1_1
-	    bin x2_0
-	    bin x2_1
-	    */
-	}
-
+        
+        sc.close();
+    }
 }
